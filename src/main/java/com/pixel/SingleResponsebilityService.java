c4d4943caaf272fb7abb21233877ee6f5089b713
sekharjpapp <43300292+sekharjpapp@users.noreply.github.com>
2023-11-03 20:10:35 +0530

    ch

--- src/main/java/com/pixel/SingleResponsebilityService.java
+++ src/main/java/com/pixel/SingleResponsebilityService.java
@@ -16,4 +16,7 @@ public class SingleResponsebilityService {
     public void freakOut() {
         System.out.println("what ever message");
     }
+    public void getSomeThing() {
+        System.out.println("getting something");
+    }
 }
